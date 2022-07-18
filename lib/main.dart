9dcecda6fd707d7edf4164c1eb726c7561547ccf
Yamato Baba <baba@infocity.co.jp>
2022-07-18 14:00:33 +0900

    GridViewでローカル配置の画像(ピカチュウ)を表示する

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,29 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var list = [
+      listItem("pikachu"),
+      listItem("pikachu"),
+      listItem("pikachu"),
+      listItem("pikachu"),
+    ];
     return MaterialApp(
       title: 'FlutterHajimeniKakuYatu',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('タイトルバーネーム'),
+          title: Text('GridViewの勉強'),
         ),
-        body: Text(
-          'テキストスタイル',
-          style: TextStyle(fontSize: 30),
-        ),
+        body: GridView.count(
+          crossAxisCount: 2
+          ,children: list,)
       ),
+    );
+  }
+
+  Widget listItem(String image) {
+    var assetImage = "assets/" + image + ".jpeg";
+    return Container(
+      child: Image.asset(assetImage, fit: BoxFit.cover,),
     );
   }
 }
